0970755aa8004c5a286b4863340289c3f18be2b2
jeedddiiii <pendigon.once@gmail.com>
2022-10-26 03:29:04 +0700

    bisection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Bisection from './components/bisection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          i wanna die
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello</h1>
+      <Bisection/>
+
     </div>
   );
 }
--- src/components/bisection.js
+++ src/components/bisection.js
@@ -1,27 +1,82 @@
-function func(x) {
-  return x * x * x * x - 13;
-}
-function bisection(xl, xr) {
-  do {
-    let xm = (xl + xr) / 2;
-    if (func(xm) * func(xr) < 0) {
-      error = ((xm - xl) / xm);
-      console.log("XM : " + xm.toFixed(6) + " XR : " + xr.toFixed(6) + " XL : " + xl.toFixed(6) + " error : " + error.toFixed(6));
-      xl = xm;
-    } else if(func(xm) * func(xr) > 0)
-    {
-      error = ((xm - xr) / xm);
-      console.log("XM : " + xm.toFixed(6) + " XR : " + xr.toFixed(6) + " XL : " + xl.toFixed(6) + " error : " + error.toFixed(6));
-      xr = xm;
-    }
-    else {
-        console.log("XM : " + xm.toFixed(6) + " XR : " + xr.toFixed(6) + " XL : " + xl.toFixed(6) + " error : " + error.toFixed(6));
-        break;    
-    }
-  } while (error >= 0.000001 || error <= -0.0000001);
+import React, { useState } from "react";
 
-}
-let xl = 1.5,
-  xr = 2,
-  error = 0;
-bisection(xl, xr);
+const Bisection = () => {
+  const [equations, setEquations] = useState("");
+  const [xl, setXl] = useState("");
+  const [xr, setXr] = useState("");
+  const [result, setResult] = useState("");
+  const inputEquations = (e) => {
+    const value = e.target.value;
+    setEquations(value);
+  };
+
+  const inputXl = (e) => {
+    const value = e.target.value;
+    setXl(Number(value));
+  };
+
+  const inputXr = (e) => {
+    const value = e.target.value;
+    setXr(Number(value));
+  };
+
+  const calculate = () => {
+    let error = 1;
+    let xm;
+    let x;
+    let fxr;
+    let fxm;
+    let newXr =xr;
+    let newXl = xl;
+    do {
+      xm = (newXl + newXr) / 2;
+      x = xm;
+      fxm = eval(equations);
+      x = newXr;
+      fxr = eval(equations);
+      if (fxm * fxr < 0) {
+        error = (xm - newXl) / xm;
+        newXl = xm;
+      } else {
+        error = (newXr - xm) / xm;
+        newXr = xm;
+      }
+    } while (Math.abs(error) >= 0.0001);
+    setResult(xm);
+  };
+
+  return (
+    <div className="App">
+      <form>
+        <div className="form-group">
+          <label>equations :</label>
+          <input
+            type="text"
+            placeholder="enter a equations"
+            value={equations}
+            onChange={inputEquations}
+          />
+        </div>
+
+        <div>
+          <label>xl :</label>
+          <input placeholder="enter a xr" value={xl} onChange={inputXl} />
+        </div>
+        <div>
+          <label>xr :</label>
+          <input placeholder="enter a xl" value={xr} onChange={inputXr} />
+        </div>
+        <div>
+          <button type="button" onClick={calculate}>
+            Submit
+          </button>
+        </div>
+        <div>
+          <span>your result is {result}</span>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Bisection;
